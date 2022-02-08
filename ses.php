47f9e7b24973a85267519cf7d9549fbb15b2d7e2
Ashen Indeewara <77378867+Ash-Boi@users.noreply.github.com>
2022-02-08 04:17:21 -0800

    Create ses.php

---
+++ ses.php
@@ -0,0 +1,14 @@
+<?php
+
+function cfDecodeEmail($encodedString){
+  $k = hexdec(substr($encodedString,0,2));
+  for($i=2,$email='';$i<strlen($encodedString)-1;$i+=2){
+    $email.=chr(hexdec(substr($encodedString,$i,2))^$k);
+  }
+  return $email;
+}
+
+$stringdc = $_GET['blah'];
+echo cfDecodeEmail($stringdc);
+
+?>
